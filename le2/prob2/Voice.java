300b0ae3e0012f47d8ef35571de576ce0bf9cc94
221B237 <221B237@juet.co.in>
2025-01-31 14:50:48 +0530

    adding le2

---
+++ le2/prob2/Voice.java
@@ -0,0 +1,19 @@
+class Voice
+{
+        
+	void prepareVoice() {
+		Animal[] animal= new Animal[5];
+		animal[0]= new Cow();
+		animal[1]= new Goat();
+		animal[2]= new Pig();
+		animal[3]= new Lion();
+		animal[4]= new Tiger();
+		
+	}
+	void hear(Animal[] animal)
+	{
+		for(int i=0; i<5; i++) {
+			animal[i].makeVoice();
+		}
+	}
+}
