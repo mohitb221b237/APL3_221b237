86f3c7dae19e30d64938338342f0aef63a94aed0
221B237 <221B237@juet.co.in>
2025-04-11 14:25:51 +0530

    adding le5

---
+++ le5/Main (1).java
@@ -0,0 +1,12 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	 Beverage b=new Whisky();
+	 b.templateMethod(30);
+	 b=new Beer();
+	 b.templateMethod(300);
+	 b=new Rum();
+	 b.templateMethod(50);
+	}
+}
---
+++ le5/Rum.java
@@ -0,0 +1,9 @@
+class Rum extends Beverage{
+    protected void addCondiment(){
+        System.out.println("Add ice");
+        
+    }
+    protected void stir(){
+        System.out.println("Stir for 30 sec");
+}
+}
